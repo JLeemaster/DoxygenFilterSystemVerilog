9adfbf27dd368de2cf520efb476fe32881155c72
Jacob Leemaster <jleems86@gmail.com>
2023-10-02 08:54:53 -0400

    Update `test/dofilter.sh` sheband to posix sh instead of csh

--- test/dofilter.sh
+++ test/dofilter.sh
@@ -1,4 +1,4 @@
-#!/bin/csh
+#!/bin/sh
 # Stupid wrapper script required because doxyfile FILTER_PATTERN doesn't allow passing an option
 # Package Mode is an experimental option and isn't recommended at this point
 ../filter/idv_doxyfilter_sv.pl --package_mode $1
